15b783a1d32e912b42fed3799619e331290ba308
Myla <liudmyla12@yahoo.fr>
2021-08-01 19:32:05 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import '../index.css'
+import img from '../assets/js.png'
+console.log('hello word')
+
+const title = document.createElement('h1')
+title.textContent = 'I love JavaScript'
+const jsImg = document.createElement('img')
+jsImg.className = 'js-image'
+jsImg.src = img
+document.body.append(title,jsImg)
